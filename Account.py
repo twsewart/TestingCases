67363184787f030d40a37873022023a947a8d03e
Tommy <twsewart@uwm.edu>
2022-04-22 20:57:02 -0500

    added files

---
+++ Account.py
@@ -0,0 +1,17 @@
+class Account(object):
+
+    def __init__(self, username=None, password=None):
+        pass
+
+    def getUsername(self):
+        return self.username
+
+    def setUsername(self, username):
+        self.username = username
+
+    def getPassword(self):
+        return self.password
+
+    def setPassword(self, password):
+        self.password
+
--- acceptance_tests/test_login_valid.py
+++ acceptance_tests/test_login_valid.py
@@ -1,24 +1,24 @@
 from django.test import TestCase
 from django.test import Client
-from Admin import username, password
-from TA import username, password
-from user import username, password
+import unittest
+import Account
+import models
+from login import Login
 
 
-class TestingD(TestCase):
+class TestLogin(unittest.TestCase):
     def setUp(self):
         self.client = Client()
-        self.user1 = Admin.objects.__init__(name='username1',password='myPassword1')
-        self.user2 = Admin.objects.__init__(name='username2', password='myPassword2')
+        self.user1 = Account.Account.__init__(self.user1,username="user1",password="password1")
 
-    def test_Admin_valid_login(self):
-        self.user1 = Admin.objects.__init__(name='username1',password='myPassword1')
-        self.assertEqual(login(user1.name,user1.password) == True,msg="Correct login info did not work")
+    def test_valid_login(self):
+        self.setUp()
+        response = self.client.post('/',{'username':'user1','password':'password1'})
+        self.assertEqual(response.url,'/Home/',msg="Valid login did not succeed")
 
-    def test_TA_valid_login(self):
-        self.user1 = TA.objects.__init__(name='username1',password='myPassword1')
-        self.assertEqual(login(user1.name,user1.password) == True,msg="Correct login info did not work")
 
-    def test_USER_valid_login(self):
-        self.user1 = user.objects.__init__(name='username1',password='myPassword1')
-        self.assertEqual(login(user1.name,user1.password) == True,msg="Correct login info did not work")
+    def test_invalid_login(self):
+        self.setUp()
+        response = self.client.post('/',{'username':'user1','password':'!passww3@ord1'})
+        self.assertEqual(response.context['message'], 'bad password',msg="Invalid login incorrectly works")
+
